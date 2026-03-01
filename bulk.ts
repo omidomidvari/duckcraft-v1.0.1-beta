337ab544b34efc0f25243ab90d354b4cdae12e21
homemovie <153278990+omidomidvari@users.noreply.github.com>
2026-02-28 18:26:20 -0800

    Add bulk.ts for flow and multjs integration

---
+++ bulk.ts
@@ -0,0 +1,8 @@
+import flowjs from {lib/flow.js}
+import multjs from {lib/flow.js}
+import flowts from {lib/flow.js}
+flowts.makebulkram(50, 2,);
+flowts.compile(flowjs);
+flowjs.render(bulk, 56, 68, 79, 65, b0, bf, tx, 7h, 7h, 8h, 89, oo, 00, 87, 66, 67);
+flowjs.rendertag( bf2form, 63, su, 55, 6s, 7r, 77, 66, 54, 12, o0, 86, 45, 88);
+multjs.use(flow);
